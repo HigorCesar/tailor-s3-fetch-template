dbbd55122447daa78aa74d1a92e2940a9220de73
Higor Ramos <higor.rufino-ramos@leaseplan.com>
2020-03-04 09:58:17 +0100

    remove non used files, and console.log

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-const fetchTemplate = require('./lib/fetch-template');
-const doNothing = () => 'default.html';
-const fetchTemplate = fetchTemplate('hello.html',doNothing);
-fetchTemplate({url:'http://localhost/hello.html'},x => x)
-.then(data => console.log(data))
-.catch(err => console.log(err))
